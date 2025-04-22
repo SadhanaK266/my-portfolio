217ecd2b6789526a19f5ca043586b1cac19d02ce
SadhanaK266 <sadhanakaruppusamy266@gmail.com>
2025-04-22 20:32:39 +0530

    updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,31 +12,33 @@ export const Contact = () => {
         <div className='contact-container'>
             <div className="contactTitle">
                 <h1>Contact Me</h1>
-                </div>
-                <p>Passionate about crafting web solutions and solving problems — feel free to connect for tech collaborations, internships, or career opportunities.</p>
-                <a href="" className='container'>
-                    <PhoneIcon sx={{'@media(max-width:768px)':{fontSize:'15px'}}}/>
-                    <div className="right">+91 - 7812824150</div>
-                </a>
-                <a href="" className='container'>
-                    <EmailIcon sx={{'@media(max-width:768px)':{fontSize:'15px'}}}/>
-                    <div className="right">sadhanakaruppusamy266@gmail.com</div>
-                </a>
-                
-                <a href="" className='container'>
-                    <LocationOnIcon sx={{'@media(max-width:768px)':{fontSize:'15px'}}}/>
-                    <div className="right">Tirupur, India</div>
-                </a>
-                <a href=""  className='icons'>
-                    <LinkedInIcon sx={{marginTop:'1.5%',marginRight: '2%',fontSize:'33px','&:hover':{color: 'rgb(200, 0, 200)'},
-                    '@media(max-width:768px)':{fontSize:'20px'}
-                }}/>
-                
-                    <GitHubIcon sx={{marginTop:'1.5%',marginRight: '2%',fontSize:'33px','&:hover':{color: 'rgb(200, 0, 200)'},
-                '@media(max-width:768px)':{fontSize:'20px'}
-                }}/>  
-                </a>
-            
+            </div>
+            <p>Passionate about crafting web solutions and solving problems — feel free to connect for tech collaborations, internships, or career opportunities.</p>
+            <a href="" className='container'>
+                <PhoneIcon sx={{ '@media(max-width:768px)': { fontSize: '15px' } }} />
+                <div className="right">+91 - 7812824150</div>
+            </a>
+            <a href="" className='container'>
+                <EmailIcon sx={{ '@media(max-width:768px)': { fontSize: '15px' } }} />
+                <div className="right">sadhanakaruppusamy266@gmail.com</div>
+            </a>
+
+            <a href="" className='container'>
+                <LocationOnIcon sx={{ '@media(max-width:768px)': { fontSize: '15px' } }} />
+                <div className="right">Tirupur, India</div>
+            </a>
+            <a href="" className='icons'>
+                <LinkedInIcon sx={{
+                    marginTop: '1.5%', marginRight: '2%', fontSize: '33px', '&:hover': { color: 'rgb(200, 0, 200)' },
+                    '@media(max-width:768px)': { fontSize: '20px' }
+                }} />
+
+                <GitHubIcon sx={{
+                    marginTop: '1.5%', marginRight: '2%', fontSize: '33px', '&:hover': { color: 'rgb(200, 0, 200)' },
+                    '@media(max-width:768px)': { fontSize: '20px' }
+                }} />
+            </a>
+
         </div>
     )
 }
